d8716bc092ac8d0027801dd846b826f3b7d6b6e9
RIKU <91332856+kw35670@users.noreply.github.com>
2022-05-29 01:11:12 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -81,51 +81,51 @@ jQuery('.header_nav li a').click(function() {
   return false;
 });
 
-var header = jQuery('.header').innerHeight();
+// var header = jQuery('.header').innerHeight();
 
-$(function() {
-    // ナビゲーションのリンクを指定
-   var navLink = $('.header_nav li a');
+// $(function() {
+//     // ナビゲーションのリンクを指定
+//    var navLink = $('.header_nav li a');
  
-    // 各コンテンツのページ上部からの開始位置と終了位置を配列に格納しておく
-   var contentsArr = new Array();
-  for (var i = 0; i < navLink.length; i++) {
-       // コンテンツのIDを取得
-      var targetContents = navLink.eq(i).attr('href');
-      // ページ内リンクでないナビゲーションが含まれている場合は除外する
-      if(targetContents.charAt(0) == '#') {
-         // ページ上部からコンテンツの開始位置までの距離を取得
-            var targetContentsTop = $(targetContents).offset().top - header - 1;
-         // ページ上部からコンテンツの終了位置までの距離を取得
-            var targetContentsBottom = targetContentsTop + $(targetContents).outerHeight(true);
-         // 配列に格納
-            contentsArr[i] = [targetContentsTop, targetContentsBottom]
-      }
-   };
+//     // 各コンテンツのページ上部からの開始位置と終了位置を配列に格納しておく
+//    var contentsArr = new Array();
+//   for (var i = 0; i < navLink.length; i++) {
+//        // コンテンツのIDを取得
+//       var targetContents = navLink.eq(i).attr('href');
+//       // ページ内リンクでないナビゲーションが含まれている場合は除外する
+//       if(targetContents.charAt(0) == '#') {
+//          // ページ上部からコンテンツの開始位置までの距離を取得
+//             var targetContentsTop = $(targetContents).offset().top - header - 1;
+//          // ページ上部からコンテンツの終了位置までの距離を取得
+//             var targetContentsBottom = targetContentsTop + $(targetContents).outerHeight(true);
+//          // 配列に格納
+//             contentsArr[i] = [targetContentsTop, targetContentsBottom]
+//       }
+//    };
 
  
-  // 現在地をチェックする
-   function currentCheck() {
-       // 現在のスクロール位置を取得
-        var windowScrolltop = $(window).scrollTop();
-        for (var i = 0; i < contentsArr.length; i++) {
-           // 現在のスクロール位置が、配列に格納した開始位置と終了位置の間にあるものを調べる
-          if(contentsArr[i][0] <= windowScrolltop && contentsArr[i][1] >= windowScrolltop) {
-                // 開始位置と終了位置の間にある場合、ナビゲーションにclass="current"をつける
-               navLink.removeClass('is-active');
-               navLink.eq(i).addClass('is-active');
-                i == contentsArr.length;
-          } else if (windowScrolltop < contentsArr[0][0]){
-              navLink.removeClass('is-active');
-          }
-       };
-  }
+//   // 現在地をチェックする
+//    function currentCheck() {
+//        // 現在のスクロール位置を取得
+//         var windowScrolltop = $(window).scrollTop();
+//         for (var i = 0; i < contentsArr.length; i++) {
+//            // 現在のスクロール位置が、配列に格納した開始位置と終了位置の間にあるものを調べる
+//           if(contentsArr[i][0] <= windowScrolltop && contentsArr[i][1] >= windowScrolltop) {
+//                 // 開始位置と終了位置の間にある場合、ナビゲーションにclass="current"をつける
+//                navLink.removeClass('is-active');
+//                navLink.eq(i).addClass('is-active');
+//                 i == contentsArr.length;
+//           } else if (windowScrolltop < contentsArr[0][0]){
+//               navLink.removeClass('is-active');
+//           }
+//        };
+//   }
  
-   // ページ読み込み時とスクロール時に、現在地をチェックする
-  $(window).on('load scroll', function() {
-      currentCheck();
-  });
-});
+//    // ページ読み込み時とスクロール時に、現在地をチェックする
+//   $(window).on('load scroll', function() {
+//       currentCheck();
+//   });
+// });
 
 
 /*==================
